433ce26dc76346a2c37afaedf74aab9d9789effb
fatmakhv <fatmaba@gmail.com>
2023-08-24 21:09:10 +0100

    styled-components is added

--- src/app/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/app/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -10,24 +10,33 @@ type Props = {
 
 const CourseInput: FC<Props> = ({ onAddGoal }): JSX.Element => {
     const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (e.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
         setEnteredValue(e.target.value);
     };
 
     const formSubmitHandler = (e: FormEvent) => {
         e.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
         onAddGoal(enteredValue);
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label>Course Goal
+            <div className={`form-control invalid`}>
+                <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal
                     <input type="text" onChange={goalInputChangeHandler} />
                 </label>
             </div>
-            <Button type="submit">Add Goal</Button>
+            <Button>Add Goal</Button>
         </form>
     );
 };
--- src/app/components/UI/Button.tsx
+++ src/app/components/UI/Button.tsx
@@ -1,19 +1,25 @@
-"use client";
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import styled, { IStyledComponent } from 'styled-components';
 
-import { FC, ReactNode } from 'react';
-import './Button.css';
+const Button: IStyledComponent<"web", DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>> = styled.button`
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
 
-type Props = {
-    type: "submit";
-    children: ReactNode;
-};
+    &:focus {
+        outline: none;
+    }
 
-const Button: FC<Props> = ({ type, children }): JSX.Element => {
-    return (
-        <button type={type} className="button">
-            {children}
-        </button>
-    );
-};
+    &:hover,
+    &:active {
+        background: #ac0e77;
+        border-color: #ac0e77;
+        box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    }
+`;
 
 export default Button;
