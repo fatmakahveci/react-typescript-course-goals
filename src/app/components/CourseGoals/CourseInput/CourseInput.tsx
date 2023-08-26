5616fafd6eeb52db1e79bf9e6e5619fb31e0eeb3
fatmakhv <fatmaba@gmail.com>
2023-08-26 23:57:47 +0300

    fix

--- src/app/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/app/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,14 +1,43 @@
 "use client";
 
-import { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { ButtonHTMLAttributes, ChangeEvent, DetailedHTMLProps, FC, FormEvent, useState } from 'react';
 import Button from '../../UI/Button';
 import './CourseInput.css';
+import styled, { IStyledComponent } from "styled-components";
 
 type Props = {
     onAddGoal: Function;
+    invalid: boolean;
 };
 
-const CourseInput: FC<Props> = ({ onAddGoal }): JSX.Element => {
+const FormControl: IStyledComponent<"web", DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>> = styled.div`
+    margin: 0.5rem 0;
+
+    & label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
+        color: ${props => (props.invalid ? 'red' : 'black')};
+    }
+  
+    & input {
+        display: block;
+        width: 100%;
+        border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+        background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
+    }
+    
+    & input:focus {
+        outline: none;
+        background: #fad0ec;
+        border-color: #8b005d;
+    }
+`;
+
+const CourseInput: FC<Props> = ({ onAddGoal, invalid }): JSX.Element => {
     const [enteredValue, setEnteredValue] = useState('');
     const [isValid, setIsValid] = useState(true);
 
@@ -31,11 +60,11 @@ const CourseInput: FC<Props> = ({ onAddGoal }): JSX.Element => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`form-control invalid`}>
+            <FormControl invalid={!isValid}>
                 <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal
                     <input type="text" onChange={goalInputChangeHandler} />
                 </label>
-            </div>
+                </FormControl>
             <Button>Add Goal</Button>
         </form>
     );
